cc3f9f117bbd01653c41aa717ac4574dd9b23d88
Rica Insendencia <rinsendencia@gmail.com>
2023-10-05 01:22:42 +0800

    commit

--- app/src/main/java/com/example/hearduino_mobileapp/Activity_SignUp2.java
+++ app/src/main/java/com/example/hearduino_mobileapp/Activity_SignUp2.java
@@ -13,22 +13,32 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
+import com.squareup.picasso.Picasso;
 
 import java.time.Instant;
 import java.time.temporal.TemporalAdjuster;
 
 public class Activity_SignUp2 extends AppCompatActivity {
 
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+    ImageView imgview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup2);
 
-        GoogleSignInOptions gso;
-        GoogleSignInClient gsc;
+        imgview = findViewById(R.id.userIcon); //initialize image view
 
         gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
         gsc = GoogleSignIn.getClient(this,gso);
 
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+        if (acct != null) {
+            Uri personPhoto = acct.getPhotoUrl();
+            //insert to image view
+            Picasso.get().load(personPhoto).into(imgview);
+        }
     }
 }
