f4c67675a55a1409710d4d1cb1eff131943e3d74
Rica Insendencia <rinsendencia@gmail.com>
2023-10-05 02:22:51 +0800

    commit

--- app/src/main/java/com/example/hearduino_mobileapp/Activity_SignUp2.java
+++ app/src/main/java/com/example/hearduino_mobileapp/Activity_SignUp2.java
@@ -1,28 +1,42 @@
 package com.example.hearduino_mobileapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Icon;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
+import com.github.dhaval2404.imagepicker.ImagePicker;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.squareup.picasso.Picasso;
 
+import java.io.IOException;
 import java.time.Instant;
 import java.time.temporal.TemporalAdjuster;
+import java.util.Objects;
 
 public class Activity_SignUp2 extends AppCompatActivity {
 
     GoogleSignInOptions gso;
     GoogleSignInClient gsc;
     ImageView imgview;
+    Button cBtn; //change photo button
+    Uri cPic; //change picture
+    private static final int PICK_IMAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,15 +44,47 @@ public class Activity_SignUp2 extends AppCompatActivity {
         setContentView(R.layout.activity_signup2);
 
         imgview = findViewById(R.id.userIcon); //initialize image view
+        cBtn = findViewById(R.id.CHbutton); //initialize change photo button
 
         gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
         gsc = GoogleSignIn.getClient(this,gso);
 
+        //get data from the account which is signed in
         GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
         if (acct != null) {
+            //get profile picture
             Uri personPhoto = acct.getPhotoUrl();
             //insert to image view
             Picasso.get().load(personPhoto).into(imgview);
+        }
+
+        //change photo
+        cBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gallery = new Intent();
+                gallery.setType("image/*");
+                gallery.setAction(Intent.ACTION_GET_CONTENT);
+
+                startActivityForResult(Intent.createChooser(gallery, "Select Photo"), PICK_IMAGE);
+            }
+        });
+
+    }
+
+    //write on activity result for change photo
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == PICK_IMAGE && resultCode == RESULT_OK){
+            cPic = data.getData();
+            try{
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), cPic);
+                imgview.setImageBitmap(bitmap);
+            }catch (IOException e){
+                e.printStackTrace();
+            }
         }
     }
 }
