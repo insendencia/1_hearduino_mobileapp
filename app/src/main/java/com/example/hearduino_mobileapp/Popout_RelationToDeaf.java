f591e27d6f3ca7a45dad196a2bf18df9604f8bc1
Rica Insendencia <rinsendencia@gmail.com>
2023-10-06 01:20:35 +0800

    commit

--- app/src/main/java/com/example/hearduino_mobileapp/Popout_RelationToDeaf.java
+++ app/src/main/java/com/example/hearduino_mobileapp/Popout_RelationToDeaf.java
@@ -1,14 +1,29 @@
 package com.example.hearduino_mobileapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Popout_RelationToDeaf extends AppCompatActivity {
 
+    Button cBtn, bBtn; //continue and back button
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.popout_relationtodeaf);
+
+        cBtn = findViewById(R.id.contBtn); //initialize continue button
+        bBtn = findViewById(R.id.bBUTTON); //initialize back button
+
+        cBtn.setOnClickListener(view -> openNextQ()); //opens next questions
+    }
+
+    public void openNextQ(){
+        Intent i = new Intent(this, Popout_AskForAccount.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/example/hearduino_mobileapp/Popout_ReviewInformation.java
@@ -0,0 +1,14 @@
+package com.example.hearduino_mobileapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Popout_ReviewInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.popout_reviewinformation);
+    }
+}
