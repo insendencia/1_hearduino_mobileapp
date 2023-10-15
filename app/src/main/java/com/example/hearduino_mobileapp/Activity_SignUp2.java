b4d9394345f78c392b2ff303ee93ccf7a06a32fe
Rica Insendencia <rinsendencia@gmail.com>
2023-10-15 21:06:34 +0800

    commit

--- app/src/main/java/com/example/hearduino_mobileapp/Activity_SignUp2.java
+++ app/src/main/java/com/example/hearduino_mobileapp/Activity_SignUp2.java
@@ -1,56 +1,58 @@
 package com.example.hearduino_mobileapp;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Icon;
-import android.media.Image;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.github.dhaval2404.imagepicker.ImagePicker;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.squareup.picasso.Picasso;
-
-import java.io.IOException;
-import java.time.Instant;
-import java.time.temporal.TemporalAdjuster;
-import java.util.Objects;
-import java.util.ResourceBundle;
-
 public class Activity_SignUp2 extends AppCompatActivity {
 
-    TextView name, email, number;
-    Button yBtn, nBtn;
+    TextView name, email, number, cText, rText;
+    Button cBtn, signupBtn, nBtn, checkBtn, saveBtn;
+    EditText insertCode;
+
+    //spinner for relation
+    String[] relation = {"N/A", "Parent", "Child", "Sibling", "Friend", "Spouse", "Other"};
+    Spinner spinner1;
+
+    //spinner for yes or no
+    String[] choice = {" ", "Yes", "No"};
+    Spinner spinner2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup2);
 
+        //TextView
         name = findViewById(R.id.setName); //initialize set name
         email = findViewById(R.id.setEmail); //initialize set email
         number = findViewById(R.id.setNum); //initialize set number
-        yBtn = findViewById(R.id.bYes); //initialize yes button
-        nBtn = findViewById(R.id.bNo); //initialize no button
+        cText = findViewById(R.id.connectAccountText); //initialize text
+        rText = findViewById(R.id.relationTextView); //initialize text
+
+        //Button
+        cBtn = findViewById(R.id.checkButton); //initialize check button
+        signupBtn = findViewById(R.id.suBtn); //initialize sign up button
+        nBtn = findViewById(R.id.nextBtn); //initialize next
+        checkBtn = findViewById(R.id.checkButton); //initialize check button
+        saveBtn = findViewById(R.id.saveBtn); //initialize save
+
+        //Spinner
+        spinner1 = findViewById(R.id.spinner_relations); //initialize spinner relation
+        spinner2 = findViewById(R.id.spinner_YoN); //initialize spinner choice
+
+        //EditText
+        insertCode = findViewById(R.id.insertCode); //initialize input hearduino code
+
 
         //Get text from Intent
         Intent intent = getIntent();
@@ -63,7 +65,64 @@ public class Activity_SignUp2 extends AppCompatActivity {
         number.setText(getNumber);
         email.setText(getEmail);
 
-        yBtn.setOnClickListener(view -> openReviewInformation());
+        //for the choice drop down
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(Activity_SignUp2.this, R.layout.relation_list, choice);
+        adapter2.setDropDownViewResource(R.layout.relation_list);
+
+        spinner2.setAdapter(adapter2);
+
+        spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int j, long l) {
+                String value2 = adapterView.getSelectedItem().toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        nBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(spinner2.getSelectedItem() == "No"){
+                    //set visibility to visible
+                    cText.setVisibility(View.VISIBLE);
+                    insertCode.setVisibility(View.VISIBLE);
+                    checkBtn.setVisibility(View.VISIBLE);
+                    rText.setVisibility(View.VISIBLE);
+                    spinner1.setVisibility(View.VISIBLE);
+                    saveBtn.setVisibility(View.VISIBLE);
+                } else if(spinner2.getSelectedItem() == " "){
+                    Toast.makeText(getApplicationContext(), "Please select an option", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Sign Up Now", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        //for the relation drop down
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(Activity_SignUp2.this, R.layout.choice_list, relation);
+        adapter.setDropDownViewResource(R.layout.choice_list);
+
+        spinner1.setAdapter(adapter);
+
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String value = adapterView.getSelectedItem().toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                Toast.makeText(Activity_SignUp2.this, "Please select an option", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        signupBtn.setOnClickListener(v -> openReviewInformation());
+
     }
 
     public void openReviewInformation(){
@@ -71,13 +130,15 @@ public class Activity_SignUp2 extends AppCompatActivity {
         String getName = name.getText().toString();
         String getEmail = email.getText().toString();
         String getNumber = number.getText().toString();
+        String getRelation = spinner1.getSelectedItem().toString();
 
         //transfer to next activity
         Intent i = new Intent(this, Popout_ReviewInformation.class);
         i.putExtra("name", getName);
         i.putExtra("email", getEmail);
         i.putExtra("number", getNumber);
-
+        i.putExtra("relation", getRelation);
         startActivity(i);
     }
+
 }
--- app/src/main/java/com/example/hearduino_mobileapp/Popout_ReviewInformation.java
+++ app/src/main/java/com/example/hearduino_mobileapp/Popout_ReviewInformation.java
@@ -2,6 +2,8 @@ package com.example.hearduino_mobileapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -16,29 +18,37 @@ import com.google.android.gms.tasks.Task;
 
 public class Popout_ReviewInformation extends AppCompatActivity {
 
-    //Button btnSignUp;
-    TextView name, email, num;
+    Button btnSignUp;
+    TextView name, email, num, relation, setRelationText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.popout_reviewinformation);
 
+        //Text View
         name = findViewById(R.id.setName); //initialize name
         email = findViewById(R.id.setEmail); //initialize email
         num = findViewById(R.id.setNum); //initialize phone number
-        //btnSignUp = findViewById(R.id.signup); //initialize sign up
+        relation = findViewById(R.id.setRelation); //initialize relation
+        setRelationText = findViewById(R.id.setRelationText); //initialize label
+
+        //Button
+        btnSignUp = findViewById(R.id.signup); //initialize sign up
 
         //Get text from Intent
         Intent intent = getIntent();
         String getName = intent.getStringExtra("name");
         String getNumber = intent.getStringExtra("number");
         String getEmail = intent.getStringExtra("email");
+        String getRelation = intent.getStringExtra("relation");
 
         //Set Text
         name.setText(getName);
         num.setText(getNumber);
         email.setText(getEmail);
+        relation.setText(getRelation);
+
 
         //btnSignUp.setOnClickListener(view -> openWelcomeToHearduino());
     }
