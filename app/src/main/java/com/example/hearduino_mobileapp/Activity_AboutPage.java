1f06a5359b1e0b4735e00673e0c1b0f8d6ff0744
Rica Insendencia <rinsendencia@gmail.com>
2023-11-01 02:48:12 +0800

    commit

---
+++ app/src/main/java/com/example/hearduino_mobileapp/Activity_AboutPage.java
@@ -0,0 +1,14 @@
+package com.example.hearduino_mobileapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Activity_AboutPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aboutpage);
+    }
+}
